39b49cee3ed6a32bd4889d787d7a247da2ff9e60
itzik <sitzikbs@gmail.com>
2019-02-28 20:13:05 +0200

    fix fetching models and data

--- get_models_and_data.py
+++ get_models_and_data.py
@@ -4,16 +4,17 @@ import urllib.request
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
+
 def download_and_unzip(source_url, target_file, target_dir):
 
     if not os.path.exists(target_dir):
         os.makedirs(target_dir)
-
+    global downloaded
     downloaded = 0
     def show_progress(count, block_size, total_size):
         global downloaded
-        downloaded += block_size
-        print('downloading ... %d%% \r' % round(((downloaded*100.0) / total_size)))
+        downloaded = downloaded + block_size
+        print('downloading ... %d%%\r' % round(((downloaded*100.0) / total_size)))
 
     print('downloading ... \r',)
     urllib.request.urlretrieve(source_url, filename=target_file, reporthook=show_progress)
@@ -41,14 +42,14 @@ def get_point_clouds(get_original_pcpnet=False):
         download_and_unzip(source_url, target_file, target_dir)
 
         # get subset of nyu v2 and scannet for testing
-        source_url = 'https://gitlab.cs.technion.ac.il/sitzikbs/nesti-net-resources/blob/master/data/'
+        source_url = 'https://gitlab.cs.technion.ac.il/mic/nesti-net-resources/raw/master/data/nyu_scannet_pclouds.zip'
         target_dir = os.path.join(BASE_DIR, 'data/')
         target_file = os.path.join(target_dir, 'nyu_scannet_pclouds.zip')
         download_and_unzip(source_url, target_file, target_dir)
 
     else:
         # get a copy of the pcpnet dataset with noise level indicators for selected subsets
-        source_url = 'https://gitlab.cs.technion.ac.il/sitzikbs/nesti-net-resources/blob/master/'
+        source_url = 'https://gitlab.cs.technion.ac.il/mic/nesti-net-resources/raw/master/data.zip'
         target_dir = os.path.join(BASE_DIR, 'data/')
         target_file = os.path.join(target_dir, 'data.zip')
         download_and_unzip(source_url, target_file, target_dir)
@@ -58,11 +59,11 @@ def get_trained_model(fetch_all=False):
 
     print('Fetching the trained models... \n')
     if fetch_all:
-        source_url = 'https://gitlab.cs.technion.ac.il/sitzikbs/nesti-net-resources/blob/master/log/'
+        source_url = 'https://gitlab.cs.technion.ac.il/mic/nesti-net-resources/raw/master/log/all_model_logs.zip'
         target_dir = os.path.join(BASE_DIR, 'log/')
         target_file = os.path.join(target_dir, 'all_model_logs.zip')
     else:
-        source_url = 'https://gitlab.cs.technion.ac.il/sitzikbs/nesti-net-resources/blob/master/log/'
+        source_url = 'https://gitlab.cs.technion.ac.il/mic/nesti-net-resources/raw/master/log/experts.zip'
         target_dir = os.path.join(BASE_DIR, 'log/')
         target_file = os.path.join(target_dir, 'experts.zip')
 
@@ -77,6 +78,3 @@ if __name__ == '__main__':
 
     get_point_clouds(get_original_pcpnet=False)
     get_trained_model(fetch_all=False)
-
-    # repo_path='https://gitlab.cs.technion.ac.il/sitzikbs/nesti-net-resources.git'
-    # get_point_clouds_and_models_from_repo(repo_url=repo_path)
